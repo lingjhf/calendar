282626b9be03d7976a0a7606c84b9b793b3fe29a
lingjhf <lingj.jhf@outlook.com>
2022-12-01 02:54:23 +0800

    修改日期功能

--- lib/src/calendar.dart
+++ lib/src/calendar.dart
@@ -1,28 +1,135 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide CalendarDatePicker;
+
+import 'calendar_month_picker.dart';
+import 'calendar_year_picker.dart';
+import 'enums.dart';
+import 'calendar_date_picker.dart';
+import 'theme.dart';
 
 class Calendar extends StatefulWidget {
   Calendar({
     super.key,
-    required DateTime firstDate,
-    required DateTime lastDate,
-  })  : firstDate = DateUtils.dateOnly(firstDate),
-        lastDate = DateUtils.dateOnly(lastDate);
+    DateTime? initDate,
+    DateTime? currentDate,
+    this.allowDates = const [],
+    this.minDate,
+    this.maxDate,
+    this.firstDayOfWeek = CalendarWeekDay.sunday,
+    this.disable = false,
+    this.readonly = false,
+    this.mode = CalendarMode.day,
+    this.style,
+    this.onDateChange,
+    this.onDateRangeChange,
+    this.onMultipleChange,
+    this.onMultipleRangeChange,
+  })  : currentDate = currentDate ?? DateTime.now(),
+        initDate = initDate ?? DateTime.now();
 
-  final DateTime firstDate;
+  final DateTime initDate;
 
-  final DateTime lastDate;
+  final DateTime currentDate;
+
+  final List<DateTime> allowDates;
+
+  final DateTime? minDate;
+
+  final DateTime? maxDate;
+
+  //一个星期的开始
+  final CalendarWeekDay firstDayOfWeek;
+
+  //是否禁用
+  final bool disable;
+
+  //是否只读
+  final bool readonly;
+
+  final CalendarMode mode;
+
+  final CalendarStyle? style;
+
+  final ValueChanged<DateTime>? onDateChange;
+
+  final ValueChanged<DateTimeRange>? onDateRangeChange;
+
+  final ValueChanged<List<DateTime>>? onMultipleChange;
+
+  final ValueChanged<List<DateTimeRange>>? onMultipleRangeChange;
 
   @override
   State<StatefulWidget> createState() => _CalendarState();
 }
 
 class _CalendarState extends State<Calendar> {
+  late DateTime initDate;
+  CalendarMode mode = CalendarMode.day;
+
+  @override
+  void initState() {
+    initDate = widget.currentDate;
+    mode = widget.mode;
+    super.initState();
+  }
+
+  void onYearPick() {
+    setState(() {
+      mode = CalendarMode.year;
+    });
+  }
+
+  void onMonthPick() {
+    setState(() {
+      mode = CalendarMode.month;
+    });
+  }
+
+  Widget buildPicker(CalendarStyle style) {
+    switch (mode) {
+      case CalendarMode.day:
+        return CalendarDatePicker(
+          initDate: initDate,
+          currentDate: widget.currentDate,
+          style: style,
+          onYearPick: onYearPick,
+          onMonthPick: onMonthPick,
+          onChange: widget.onDateChange,
+          onDateRangeChange: widget.onDateRangeChange,
+          onMultipleChange: widget.onMultipleChange,
+        );
+      case CalendarMode.month:
+        return CalendarMonthPicker(
+          initDate: initDate,
+          currentDate: widget.currentDate,
+        );
+      case CalendarMode.year:
+        return CalendarYearPicker(
+          initDate: initDate,
+          currentDate: widget.currentDate,
+        );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(children: [
-        
-      ],),
+    final style = widget.style ??
+        Theme.of(context).extension<CalendarStyle>() ??
+        CalendarStyle();
+
+    return DefaultTextStyle(
+      style: TextStyle(color: style.primaryColor),
+      child: Container(
+        padding: style.padding,
+        decoration: BoxDecoration(
+          color: style.backgroundColor,
+          borderRadius: style.borderRadius,
+        ),
+        child: SizedBox(
+          width: 7 * 36,
+          height: 8 * 32,
+          child: buildPicker(style),
+        ),
+      ),
     );
   }
 }
