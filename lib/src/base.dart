91cbede973a62314bc7826271670953c266b10ce
lingjhf <lingj.jhf@outlook.com>
2022-12-01 01:57:13 +0800

    新增日期选择器基类

---
+++ lib/src/base.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+abstract class BasePicker extends StatefulWidget {
+  BasePicker({
+    super.key,
+    DateTime? initDate,
+    DateTime? currentDate,
+    this.onChange,
+  })  : initDate = initDate ?? DateTime.now(),
+        currentDate = currentDate ?? DateTime.now();
+
+  //初始化日期
+  final DateTime initDate;
+
+  //当前选择日期
+  //如果有当前日期就定位到当期日期，如果没有就定位到今天
+  final DateTime currentDate;
+
+  final ValueChanged<DateTime>? onChange;
+}
