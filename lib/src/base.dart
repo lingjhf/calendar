52638768c1b6f2ab38621173ac778e4a3203cff0
lingjhf <lingj.jhf@outlook.com>
2022-12-02 03:04:29 +0800

    新增样式属性

--- lib/src/base.dart
+++ lib/src/base.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
+import '../calendar.dart';
+
 abstract class BasePicker extends StatefulWidget {
   BasePicker({
     super.key,
     DateTime? initDate,
     DateTime? currentDate,
+    this.style,
     this.onChange,
   })  : initDate = initDate ?? DateTime.now(),
         currentDate = currentDate ?? DateTime.now();
@@ -15,6 +18,8 @@ abstract class BasePicker extends StatefulWidget {
   //当前选择日期
   //如果有当前日期就定位到当期日期，如果没有就定位到今天
   final DateTime currentDate;
+
+  final CalendarStyle? style;
 
   final ValueChanged<DateTime>? onChange;
 }
